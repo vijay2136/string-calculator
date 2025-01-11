87aeaae0c93b1049dbfa9138188cdf793368814d
vijay2136 <imvijay2136@gmail.com>
2025-01-11 17:49:09 +0530

    Handled new lines between numbers case

--- app/services/calculator.rb
+++ app/services/calculator.rb
@@ -4,6 +4,11 @@ class Calculator
   def self.add(string_numbers)
     return 0 if string_numbers.empty?
 
-    string_numbers.split(",").sum(&:to_i)
+    string_numbers
+      .gsub(/\n/, ',')
+      .split(',')
+      .reject(&:empty?)
+      .map(&:to_i)
+      .sum
   end
 end
--- test/services/calculator_test.rb
+++ test/services/calculator_test.rb
@@ -18,4 +18,13 @@ class CalculatorTest < ActiveSupport::TestCase
   def test_returns_sum_for_multiple_numbers
     assert_equal 55, Calculator.add("1,2,3,4,5,6,7,8,9,10")
   end
+
+  def test_returns_sum_for_multiple_numbers_with_new_line
+    assert_equal 6, Calculator.add("1\n2,3")
+  end
+
+  def test_returns_sum_correctly_even_if_the_string_ends_with_a_new_line
+    assert_equal 1, Calculator.add("1,\n")
+    assert_equal 3, Calculator.add("1,\n2,\n")
+  end
 end
