0311f172a2cbedcd46d09a7dc7dfa8f36707f0a9
vijay2136 <imvijay2136@gmail.com>
2025-01-12 17:40:17 +0530

    Rubocop fixes and code refactor

--- app/services/calculator.rb
+++ app/services/calculator.rb
@@ -6,19 +6,27 @@ class Calculator
   def self.add(string_numbers)
     return 0 if string_numbers.empty?
 
+    delimiter, string_numbers = parse_delimiter_and_numbers(string_numbers)
+
+    numbers = string_numbers.split(/[#{delimiter}\n]+/).reject(&:empty?).map(&:to_i)
+
+    negative_numbers = numbers.select(&:negative?)
+
+    if negative_numbers.any?
+      raise NegativeNumbersAreNotAllowed, "negative numbers are not allowed: #{negative_numbers.join(', ')}"
+    end
+
+    numbers.sum
+  end
+
+  private_class_method def self.parse_delimiter_and_numbers(string_numbers)
     if string_numbers.start_with?("//")
       delimiter = string_numbers[2]
-      string_numbers = string_numbers[4..-1]
+      string_numbers = string_numbers[4..]
     else
       delimiter = ","
     end
 
-    numbers = string_numbers.split(/[#{delimiter}\n]+/).reject(&:empty?).map(&:to_i)
-
-    negative_numbers = numbers.select { |number| number < 0 }
-
-    raise NegativeNumbersAreNotAllowed, "negative numbers are not allowed: #{negative_numbers.join(', ')}" if negative_numbers.any?
-
-    numbers.sum
+    [delimiter, string_numbers]
   end
 end
